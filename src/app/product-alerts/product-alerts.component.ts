97bc6c1adeeee14fd1558eb64abf93f6af45ce0b
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-08-29 10:01:01 -0500

    added notification function

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product, products } from '../products';
 
 @Component({
@@ -8,4 +8,5 @@ import { Product, products } from '../products';
 })
 export class ProductAlertsComponent {
   @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
